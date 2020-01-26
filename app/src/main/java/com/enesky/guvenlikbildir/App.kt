f06834f36981a74b74c9d6c1fa063b6ca4190669
Enes Kamil Yılmaz <eneskamilyilmaz@gmail.com>
2020-01-26 20:51:49 +0300

    Firebase modülleri eklendi.

--- app/src/main/java/com/enesky/guvenlikbildir/App.kt
+++ app/src/main/java/com/enesky/guvenlikbildir/App.kt
@@ -1,7 +1,9 @@
 package com.enesky.guvenlikbildir
 
 import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 /**
  * Created by Enes Kamil YILMAZ on 26.01.2020
@@ -18,13 +20,23 @@ class App : Application() {
         private lateinit var mAuth: FirebaseAuth
         val managerAuth: FirebaseAuth
             get() = mAuth
+
+        private lateinit var mAnalytics: FirebaseAnalytics
+        val managerAnalytics: FirebaseAnalytics
+            get() = managerAnalytics
+
+        private lateinit var mFirestore: FirebaseFirestore
+        val managerFirestore: FirebaseFirestore
+            get() = mFirestore
+
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
         mAuth = FirebaseAuth.getInstance()
-
+        mAnalytics = FirebaseAnalytics.getInstance(this)
+        mFirestore = FirebaseFirestore.getInstance()
     }
 
 }
---
+++ app/src/main/java/com/enesky/guvenlikbildir/FcmService.kt
@@ -0,0 +1,19 @@
+package com.enesky.guvenlikbildir
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FcmService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        // TODO(developer): Handle FCM messages here.
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null)
+            Log.d("FCM", "Message Notification Body: " + remoteMessage.notification!!.body)
+
+    }
+
+}
