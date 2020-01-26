30276f57ef8070ad84c4824ebe08661bf3d4e35d
Enes Kamil Yılmaz <eneskamilyilmaz@gmail.com>
2020-01-26 17:09:35 +0300

    Projenin ilk hali eklendi.

---
+++ app/src/main/java/com/enesky/guvenlikbildir/data/model/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.enesky.guvenlikbildir.data.model
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
