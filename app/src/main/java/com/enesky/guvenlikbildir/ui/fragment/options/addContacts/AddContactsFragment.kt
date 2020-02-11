4369ef38af57ffdf0a2b3e7ff6ffba0b8c51bd81
Enes Kamil YILMAZ <eneskamilyilmaz@gmail.com>
2020-02-11 16:39:32 +0300

    Cihazdaki rehber bilgilerine erişildi ve listeye eklendi.

--- app/src/main/java/com/enesky/guvenlikbildir/ui/fragment/options/addContacts/AddContactsFragment.kt
+++ app/src/main/java/com/enesky/guvenlikbildir/ui/fragment/options/addContacts/AddContactsFragment.kt
@@ -1,39 +1,35 @@
 package com.enesky.guvenlikbildir.ui.fragment.options.addContacts
 
-import android.R.attr
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Intent
-import android.os.Build
+import android.content.Context
+import android.database.Cursor
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.enesky.guvenlikbildir.R
 import com.enesky.guvenlikbildir.databinding.FragmentAddContactsBinding
 import com.enesky.guvenlikbildir.extensions.getViewModel
 import com.enesky.guvenlikbildir.extensions.requireReadContactsPermission
 import com.enesky.guvenlikbildir.ui.fragment.BaseFragment
+import io.github.farhad.contactpicker.ContactPicker
 import kotlinx.android.synthetic.main.fragment_add_contacts.*
 
-class AddContactsFragment: BaseFragment() {
+class AddContactsFragment : BaseFragment() {
 
     private lateinit var addContactsVM: AddContactsVM
     private lateinit var binding: FragmentAddContactsBinding
 
-    @SuppressLint("InlinedApi")
-    private val FROM_COLUMNS: Array<String> = arrayOf(
-        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)) {
-            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
-        } else {
-            ContactsContract.Contacts.DISPLAY_NAME
-        }
-    )
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_contacts, container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_add_contacts, container, false)
         return binding.root
     }
 
@@ -54,39 +50,38 @@ class AddContactsFragment: BaseFragment() {
         btn.setOnClickListener {
             requireContext().requireReadContactsPermission {
 
-                /*val contactPickerIntent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
-                startActivityForResult(contactPickerIntent, 1)*/
+                /*val picker: ContactPicker? = ContactPicker.create(
+                    activity = requireActivity() as AppCompatActivity,
+                    onContactPicked = { Log.d("TAG", it.name + ": " + it.number) },
+                    onFailure = { Log.d("TAG", it.localizedMessage) })
 
-                launchMultiplePhonePicker()
+                picker!!.pick()*/
 
+                getContactsList(requireContext())
             }
         }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    data class Contact(val number: String, val name: String?)
 
-        when (requestCode){
-            REQUEST_CODE_PICK_CONTACT -> {
-                 if (resultCode == Activity.RESULT_OK) {
-                     val bundle: Bundle = data!!.extras!!
-                     val result = bundle.get("result")
-                 }
-             }
+    fun getContactsList(context: Context) {
+        val contacts: ArrayList<Contact> = ArrayList()
+        val phones: Cursor? = context.contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            null, null, null, null)
+
+        while (phones!!.moveToNext()) {
+            val name: String = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+            val phoneNumber: String = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)).replace(" ", "")
+            val itype = phones.getInt(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE))
+            val isMobile = itype == ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE || itype == ContactsContract.CommonDataKinds.Phone.TYPE_WORK_MOBILE
+
+            if(name.isNotEmpty() && phoneNumber.isNotEmpty())
+                if (isMobile && !contacts.contains(Contact(name, phoneNumber)))
+                    contacts.add(Contact(name, phoneNumber))
         }
-
-    }
-
-    val REQUEST_CODE_PICK_CONTACT = 1
-    val MAX_PICK_CONTACT = 10
-
-    private fun launchMultiplePhonePicker() {
-        val phonebookIntent = Intent("intent.action.INTERACTION_TOPMENU")
-        phonebookIntent.putExtra("additional", "phone-multi")
-        phonebookIntent.putExtra("maxRecipientCount", MAX_PICK_CONTACT)
-        phonebookIntent.putExtra("FromMMS", true)
-        startActivityForResult(phonebookIntent, REQUEST_CODE_PICK_CONTACT)
+        phones.close()
+        //return contacts
     }
 
 }
