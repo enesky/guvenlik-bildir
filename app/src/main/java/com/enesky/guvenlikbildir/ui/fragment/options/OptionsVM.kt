ad65ee7ed6b48db96a88fc6b2107ea8f0daae244
Enes Kamil YILMAZ <eneskamilyilmaz@gmail.com>
2020-06-01 15:31:57 +0300

    Uygulama Hakkında metni eklendi. Uygulamayı Puanla - Yorumla ikonu güncellendi.

--- app/src/main/java/com/enesky/guvenlikbildir/ui/fragment/options/OptionsVM.kt
+++ app/src/main/java/com/enesky/guvenlikbildir/ui/fragment/options/OptionsVM.kt
@@ -23,7 +23,7 @@ class OptionsVM : BaseViewModel(), OptionAdapter.OptionListListener {
             OptionItem(notificationResIdLive.value!!, getString(R.string.item_option_10)),
             //OptionItem(R.drawable.ic_info, getString(R.string.item_option_2)),
             //OptionItem(R.drawable.ic_phone, getString(R.string.item_option_3)),
-            OptionItem(R.drawable.ic_about, getString(R.string.item_option_4)),
+            OptionItem(R.drawable.ic_shop, getString(R.string.item_option_4)),
             OptionItem(R.drawable.ic_share_grey, getString(R.string.item_option_5)),
             OptionItem(R.drawable.ic_mail, getString(R.string.item_option_6)),
             OptionItem(R.drawable.ic_github, getString(R.string.item_option_7)),
